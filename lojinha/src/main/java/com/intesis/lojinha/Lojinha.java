e4abfbcd28c2fef05ec93575f9624aa27f4e8803
joaorsantos1136 <166754257+joaorsantos1136@users.noreply.github.com>
2024-04-12 12:05:48 -0300

    Add files via upload

---
+++ lojinha/src/main/java/com/intesis/lojinha/Lojinha.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.intesis.lojinha;
+
+/**
+ *
+ * @author joaopaulo.santos
+ */
+public class Lojinha {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
